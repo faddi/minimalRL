8b19191706e6ac3a8a9f509fca30877d874348ed
faddi <faddi.idris@gmail.com>
2020-08-21 20:42:49 +0200

    mid

--- dqn.py
+++ dqn.py
@@ -3,31 +3,25 @@ import collections
 import random
 
 import torch
-from torch import argmax
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.nn.utils.prune as prune
-
-from typing import List
-from torch.optim import optimizer
 
 from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter()
-
 
 # import numpy as np
 
 import matplotlib.pyplot as plt
 
 # Hyperparameters
-learning_rate = 1e-2
-gamma = 0.999
+learning_rate = 1e-4
+gamma = 0.95
 buffer_limit = 10000
-batch_size = 64
+batch_size = 1
 random_steps = 0
-num_nets = 5
+num_nets = 2
 
 
 class GaussianNoise(nn.Module):
@@ -86,6 +80,9 @@ class ReplayBuffer:
     def put(self, transition):
         self.buffer.append(transition)
 
+    def clear(self):
+        self.buffer.clear()
+
     def set_max_reward(self, r):
         if self.max_reward is None:
             self.max_reward = r
@@ -116,26 +113,16 @@ class ReplayBuffer:
         return len(self.buffer)
 
 
-def nl(x):
-    # return torch.log(torch.abs(x) + 0.1)
-    # return torch.nn.functional.leaky_relu(x)
-    # return torch.sigmoid(x)
-    # return torch.tanh(x)
-    return torch.relu(x)
-    # return torch.sin(x)
-    # return torch.relu(torch.fmod(x, 2))
-    # return torch.tanh(torch.fmod(x, 2))
-
-
 class Qnet(nn.Module):
     def __init__(self, env: gym.Env, memory: ReplayBuffer):
         super(Qnet, self).__init__()
         self.memory = memory
-        h = 32
-        self.noise = GaussianNoise(sigma=0.01, is_relative_detach=False)
+        h = 256
+        self.noise = GaussianNoise(sigma=0.1, is_relative_detach=False)
         # self.fc1 = nn.Linear(4, h)
         self.inp = env.observation_space.shape[0]
         self.outp = env.action_space.n
+        self.noise = GaussianNoise(0.05)
 
         self.fc1 = nn.Linear(self.inp, h)
         self.fc2 = nn.Linear(h, h)
@@ -143,9 +130,12 @@ class Qnet(nn.Module):
 
     def forward(self, x):
 
-        x = torch.relu(self.fc1(x))
-        # x = torch.tanh(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc1(x))
+        # x = self.noise(x)
+        x = F.relu(self.fc2(x))
+        # x = self.noise(x)
+        x = self.fc3(x)  # * 500
+        # x = F.sigmoid(x) * 1 * 500  # self.memory.max_reward
         return x
 
     def sample_action(self, obs, epsilon, mem_size):
@@ -158,95 +148,55 @@ class Qnet(nn.Module):
             return out.argmax().item()
 
 
-def train(q, q_target, memory, optimizer, ep):
+def train_(qs, memory, optimizers, ep):
     losses = []
     if memory.size() < random_steps:
         return
 
-    # bz = batch_size if memory.max_reward < 400 else batch_size * 8
-    # bz = int(memory.max_reward) * 2
     bz = batch_size
+    target_index = torch.randint(0, len(qs), ())
+    q = qs[target_index]
+    optimizer = optimizers[target_index]
 
-    for i in range(400):
+    for i in range(800):
         s, a, r, s_prime, done_mask = memory.sample(bz)
-
-        # n = torch.distributions.Uniform(0.99, 1.010).sample()
-
-        # n = (torch.rand_like(s) - 0.5) * 0.5
-
-        # s = s + n
-        # s_prime = s_prime + n
 
         q_out = q(s)
         q_a = q_out.gather(1, a)  # / memory.max_reward
         # max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
         with torch.no_grad():
 
-            # max_q_prime1 = q_target(s_prime).max(1)[0]
-            # max_q_prime2 = q(s_prime).max(1)[0]
-            # max_q = torch.stack((max_q_prime1, max_q_prime2), dim=1)
-            # max_q_prime = max_q.min(1)[0].unsqueeze(1)
+            # q_min = qs[0](s_prime)
 
-            max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
+            # for i in range(1, len(qs)):
+            #     m = qs[i](s_prime)
+            #     q_min = torch.min(m, q_min)
 
-            # max_q_prime1 = q_target(s_prime).max(1)[0].unsqueeze(1)
-            # max_q_prime = q_target(s_prime).mean(1).unsqueeze(1)
+            q_avg = qs[0](s_prime)
+
+            for i in range(1, len(qs)):
+                m = qs[i](s_prime)
+                q_avg += m
+
+            q_avg = q_avg / len(qs)
+
+            max_q_prime = q_a.max(1)[0].unsqueeze(1)
             target = r + gamma * max_q_prime * done_mask
 
-            target[target > 500.0] = torch.tensor(500.0)
+            # target[target > 500.0] = torch.tensor(500.0)
             # target = torch.log(target + 1)
             # target = target / memory.max_reward
 
         # loss = F.smooth_l1_loss(q_a, target)
-        # l = F.mse_loss(q_a, target, reduce=None)
-        l = (q_a - target) ** 2
-        # m = l.max()
-        # l[l < 0.01] = 0
-        loss = l.mean()
+        loss = F.mse_loss(q_a, target)
+        # l = (q_a - target) ** 2
+        # loss = l.mean()
         # loss = torch.sigmoid(q_a - target).mean()
 
         optimizer.zero_grad()
         loss.backward()
 
-        # q.fc1.weights
-
-        # plot_grad_flow(q.named_parameters())
-        # clip = 1.0 / (memory.max_reward ** 2)
-        # for p in q.parameters():
-        #     clip = torch.log(p.grad.abs().max() + 1)
-        #     p.grad.data.clamp_(-clip, clip)
-
-        # for p in q.parameters():
-        #     if p.grad is None or len(p.grad.shape) == 1:
-        #         continue
-
-        #     if torch.randint(low=0, high=10, size=()) >= 5:
-        #         r = torch.zeros_like(p.grad)
-        #         p.grad *= r
-
-        #     # idx = torch.argmax(p.grad, dim=1)
-        #     # idx = torch.randint(0, p.grad.shape[1], (p.grad.shape[0], 1))
-
-        #     # # p.grad.where(p.grad <= value, p.grad * 0.0001, p.grad)
-        #     # r = torch.zeros_like(p.grad)
-        #     # if torch.randint(low=0, high=10, size=()) >= 5:
-        #     #     r[:, idx] = 1.0
-
-        #     # p.grad *= r
-
-        #     # for i, qq in enumerate(p.grad):
-        #     #     if i == idx:
-        #     #         qq *= 0.0001
-
-        #     # half = len(p.grad) // 2
-        #     # if ep % 10 > 5:
-        #     #     p.grad[:half] *= 0.001  # or whatever other operation
-        #     # else:
-        #     #     p.grad[half:] *= 0.001  # or whatever other operation
-
         optimizer.step()
-        # q.fc1.weight.data = torch.clamp(q.fc1.weight.data, -1.0, 1.0)
-        # q.fc2.weight.data = torch.clamp(q.fc2.weight.data, -1.0, 1.0)
         losses.append(loss.item())
 
     writer.add_scalar("Loss/train", torch.mean(torch.tensor(losses)), ep)
@@ -258,8 +208,9 @@ def main():
     env = gym.make("CartPole-v1")
     # env = gym.make("LunarLander-v2")
 
-    memories = [ReplayBuffer() for _ in range(num_nets)]
-    qs = [Qnet(env, memories[i]) for i in range(num_nets)]
+    # memories = [ReplayBuffer() for _ in range(num_nets)]
+    memory = ReplayBuffer()
+    qs = [Qnet(env, memory) for i in range(num_nets)]
 
     def weights_init_uniform(m):
         classname = m.__class__.__name__
@@ -278,11 +229,11 @@ def main():
     # q_target.apply(weights_init_uniform)
     # q_target.load_state_dict(q.state_dict())
 
-    for i, q in enumerate(qs):
-        if i == 0:
-            continue
+    # for i, q in enumerate(qs):
+    #     if i == 0:
+    #         continue
 
-        q.load_state_dict(qs[0].state_dict())
+    #     q.load_state_dict(qs[0].state_dict())
 
     # clip_value = 0.3
     # for p in q.parameters():
@@ -318,8 +269,19 @@ def main():
 
             obs = torch.from_numpy(s).float().unsqueeze(0)
 
-            outs = [q(obs).argmax().item() for q in qs]
-            a = max(set(outs), key=outs.count)
+            # outs = [q(obs).argmax().item() for q in qs]
+            # a = max(set(outs), key=outs.count)
+
+            # m = qs[0](obs)
+            # for i in range(1, len(qs)):
+            #     m2 = qs[i](obs)
+            #     m = torch.max(m2, m)
+
+            o = qs[0](obs)
+            for i in range(1, len(qs)):
+                o += qs[i](obs)
+
+            a = o.argmax().item()
 
             # outs = torch.stack([q(obs) for q in qs])
             # outs = torch.sum(outs, 0)
@@ -330,20 +292,22 @@ def main():
             env.render()
             done_mask = 0.0 if done else 1.0
 
-            data = (s, a, r / 500, s_prime, done_mask)
+            data = (s, a, r, s_prime, done_mask)
             # if done:
             #     data = (s, a, score / 500, s_prime, done_mask)
             # else:
             #     data = (s, a, 0.0, s_prime, done_mask)
 
-            memories[step % len(memories)].put(data)
+            # memories[step % len(memories)].put(data)
+            memory.put(data)
 
             s = s_prime
 
             score += r
             step += 1
             if done:
-                [m.set_max_reward(score) for m in memories]
+                # [m.set_max_reward(score) for m in memories]
+                memory.set_max_reward(score)
                 writer.add_scalar("reward", score, n_epi)
                 break
 
@@ -361,25 +325,14 @@ def main():
             # q_target.load_state_dict(q.state_dict())
             print(
                 "n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
-                    n_epi, score / print_interval, memories[0].size(), epsilon * 100
+                    n_epi, score / print_interval, memory.size(), epsilon * 100
                 )
             )
             score = 0.0
 
-        for i in range(num_nets):
-            q = qs[i]
-
-            r = int(torch.randint(0, len(memories), ()).item())
-            r2 = int(torch.randint(0, len(memories), ()).item())
-
-            q_target = qs[r]
-            memory = memories[r2]
-
-            # q_target = qs[(i + 1) % len(qs)]
-            # memory = memories[i]
-            optimizer = optimizers[i]
-
-            train(q, q_target, memory, optimizer, n_epi)
+        # if memory.size() > 100:
+        train_(qs, memory, optimizers, n_epi)
+        # memory.clear()
 
     env.close()
 
